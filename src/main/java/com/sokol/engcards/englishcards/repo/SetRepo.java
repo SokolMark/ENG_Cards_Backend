4cd6645d300a533730d62598f17fb218902201f8
Mark <159955291+SokolMark@users.noreply.github.com>
2025-07-12 22:36:23 +0300

    A request for a specific set has been implemented

--- src/main/java/com/sokol/engcards/englishcards/repo/SetRepo.java
+++ src/main/java/com/sokol/engcards/englishcards/repo/SetRepo.java
@@ -2,6 +2,9 @@ package com.sokol.engcards.englishcards.repo;
 
 import com.sokol.engcards.englishcards.entity.WordSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SetRepo extends JpaRepository<WordSet, Integer> {
+    @Query(value = "SELECT * FROM word_set WHERE name = ?", nativeQuery = true)
+    WordSet getSetByName(String name);
 }

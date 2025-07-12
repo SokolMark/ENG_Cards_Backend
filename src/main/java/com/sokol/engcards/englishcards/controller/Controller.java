7c49bba2178aaf4ad5c7712f315d6eebe8f65aa7
SokolMark <sarkkofag@gmail.com>
2025-07-12 18:05:08 +0300

    implemented query for all sets and words

--- src/main/java/com/sokol/engcards/englishcards/controller/Controller.java
+++ src/main/java/com/sokol/engcards/englishcards/controller/Controller.java
@@ -1,9 +1,14 @@
 package com.sokol.engcards.englishcards.controller;
 
+import com.sokol.engcards.englishcards.entity.Word;
+import com.sokol.engcards.englishcards.entity.WordSet;
 import com.sokol.engcards.englishcards.service.CardService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -11,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     private final CardService cardService;
 
+    @GetMapping("/words")
+    public List<Word> getWords() {
+        return cardService.findAllWords();
+    }
+
+    @GetMapping("/Sets")
+    public List<WordSet> getSets() {
+        return cardService.findAllSets();
+    }
 }
--- src/main/java/com/sokol/engcards/englishcards/service/CardService.java
+++ src/main/java/com/sokol/engcards/englishcards/service/CardService.java
@@ -1,13 +1,25 @@
 package com.sokol.engcards.englishcards.service;
 
+import com.sokol.engcards.englishcards.entity.Word;
+import com.sokol.engcards.englishcards.entity.WordSet;
 import com.sokol.engcards.englishcards.repo.SetRepo;
 import com.sokol.engcards.englishcards.repo.WordRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class CardService {
     private WordRepo wordRepo;
     private SetRepo setRepo;
+
+    public List<Word> findAllWords() {
+        return wordRepo.findAll();
+    }
+
+    public List<WordSet> findAllSets() {
+        return setRepo.findAll();
+    }
 }
